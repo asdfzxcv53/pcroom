ced76d3ff14b9e2c627de348a0b5d899e9c69c61
asdfzxcv53 <sskij@naver.com>
2026-01-16 00:21:06 +0900

    error 처리

--- src/main/java/com/example/pcroom/presentation/controller/AuthController.java
+++ src/main/java/com/example/pcroom/presentation/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.example.pcroom.presentation.controller;
 
 import com.example.pcroom.application.AuthService;
 import com.example.pcroom.application.LogoutService;
-import com.example.pcroom.infrastructure.security.JwtUtil;
 import com.example.pcroom.presentation.LoginRequestDto;
 import com.example.pcroom.presentation.LoginResponseDto;
 import com.example.pcroom.presentation.LoginResult;
@@ -12,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
@@ -21,17 +18,11 @@ import java.time.Duration;
 @RestController
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
-    private final UserDetailsService userDetailsService;
     private final AuthService authService;
     private final LogoutService logoutService;
 
     @Autowired
-    public AuthController (AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserDetailsService userDetailsService, AuthService authService, LogoutService logoutService) {
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-        this.userDetailsService = userDetailsService;
+    public AuthController (AuthService authService, LogoutService logoutService) {
         this.authService = authService;
         this.logoutService = logoutService;
     }
@@ -55,7 +46,7 @@ public class AuthController {
 
         response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
 
-        LoginResponseDto loginResponseDto = new LoginResponseDto(loginResult.getUserSummary(), accessToken)
+        LoginResponseDto loginResponseDto = new LoginResponseDto(loginResult.getUserSummary(), accessToken);
 
         return ResponseEntity.ok(loginResponseDto);
     }
