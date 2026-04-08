86a821f04f60926dccbf831b6aaba487efd0bcca
asdfzxcv53 <sskij@naver.com>
2026-04-08 22:27:52 +0900

    [add] corsconfig

--- src/main/java/com/example/pcroom/infrastructure/initializer/RedisRefreshTokenDataLoader.java
+++ src/main/java/com/example/pcroom/infrastructure/initializer/RedisRefreshTokenDataLoader.java
@@ -1,45 +1,45 @@
-package com.example.pcroom.infrastructure.initializer;
-
-import com.example.pcroom.domain.RefreshToken;
-import com.example.pcroom.domain.Role;
-import com.example.pcroom.domain.User;
-import com.example.pcroom.infrastructure.RefreshTokenRepository;
-import com.example.pcroom.infrastructure.UserRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
-import java.time.Duration;
-
-@Component
-@Profile("redis")
-public class RedisRefreshTokenDataLoader implements CommandLineRunner {
-
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final UserRepository userRepository;
-
-    @Autowired
-    public RedisRefreshTokenDataLoader(RefreshTokenRepository refreshTokenRepository, UserRepository userRepository) {
-        this.refreshTokenRepository = refreshTokenRepository;
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-        for(int i = 1; i <= 20000; i++){
-            User user = new User("asdf"+i, "1234", "User"+i, "010"+i, Role.USER);
-            User savedUser = userRepository.save(user);
-
-            RefreshToken refreshToken = RefreshToken.create(
-                    user.getId(),
-                    "hashed_token"+savedUser.getId(),
-                    Duration.ofDays(14)
-            );
-
-            refreshTokenRepository.save(refreshToken);
-        }
-    }
-}
+//package com.example.pcroom.infrastructure.initializer;
+//
+//import com.example.pcroom.domain.RefreshToken;
+//import com.example.pcroom.domain.Role;
+//import com.example.pcroom.domain.User;
+//import com.example.pcroom.infrastructure.RefreshTokenRepository;
+//import com.example.pcroom.infrastructure.UserRepository;
+//import jakarta.transaction.Transactional;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.Duration;
+//
+//@Component
+//@Profile("redis")
+//public class RedisRefreshTokenDataLoader implements CommandLineRunner {
+//
+//    private final RefreshTokenRepository refreshTokenRepository;
+//    private final UserRepository userRepository;
+//
+//    @Autowired
+//    public RedisRefreshTokenDataLoader(RefreshTokenRepository refreshTokenRepository, UserRepository userRepository) {
+//        this.refreshTokenRepository = refreshTokenRepository;
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void run(String... args) throws Exception {
+//        for(int i = 1; i <= 20000; i++){
+//            User user = new User("asdf"+i, "1234", "User"+i, "010"+i, Role.USER);
+//            User savedUser = userRepository.save(user);
+//
+//            RefreshToken refreshToken = RefreshToken.create(
+//                    user.getId(),
+//                    "hashed_token"+savedUser.getId(),
+//                    Duration.ofDays(14)
+//            );
+//
+//            refreshTokenRepository.save(refreshToken);
+//        }
+//    }
+//}
--- src/main/java/com/example/pcroom/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/example/pcroom/infrastructure/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/user", "/login", "/remainTime", "/refreshToken").permitAll()
