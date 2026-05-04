af83ea27733f85f4c44d4644070577a557de4e54
asdfzxcv53 <sskij@naver.com>
2026-05-04 15:12:48 +0900

    refactor: change ready request and change some logic

--- src/main/java/com/example/pcroom/application/KakaoPayService.java
+++ src/main/java/com/example/pcroom/application/KakaoPayService.java
@@ -8,18 +8,13 @@ import com.example.pcroom.infrastructure.kakao.KakaoPayClient;
 import com.example.pcroom.presentation.kakao.*;
 import com.example.pcroom.presentation.orders.OrdersCancelResponse;
 import com.example.pcroom.presentation.orders.OrdersResponseDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 
-import java.io.IOException;
-
 @Service
-@Transactional
 @Slf4j
 public class KakaoPayService {
 
@@ -28,31 +23,25 @@ public class KakaoPayService {
     private final PaymentProcessService paymentProcessService;
     private final KakaoPayProperties kakaoPayProperties;
     private final OrdersRepository ordersRepository;
-    private final ObjectMapper objectMapper;
     private final KakaoPayClient kakaoPayClient;
 
     @Autowired
-    public KakaoPayService(KakaoPayStateService kakaoPayStateService, KakaoTidRepository tidRepository, PaymentProcessService paymentProcessService, KakaoPayProperties kakaoPayProperties, OrdersRepository ordersRepository, KakaoPayClient kakaoPayClient, ObjectMapper objectMapper) {
+    public KakaoPayService(KakaoPayStateService kakaoPayStateService, KakaoTidRepository tidRepository, PaymentProcessService paymentProcessService, KakaoPayProperties kakaoPayProperties, OrdersRepository ordersRepository, KakaoPayClient kakaoPayClient) {
         this.kakaoPayStateService = kakaoPayStateService;
         this.tidRepository = tidRepository;
         this.paymentProcessService = paymentProcessService;
         this.kakaoPayProperties = kakaoPayProperties;
         this.ordersRepository = ordersRepository;
-        this.objectMapper = objectMapper;
         this.kakaoPayClient = kakaoPayClient;
     }
 
     // 실제 대기상태인 주문을 만든 후에 카카오페이에 ready api 를 보낸다.
-    public KakaoReadyResponse ready(OrdersResponseDto ordersResponseDto) {
+    @Transactional
+    public KakaoReadyResponse ready(Long orderId) {
         log.info("[KakaoPay] ready start ordersId = {}",
-                ordersResponseDto.getOrderId());
+                orderId);
 
-        Orders orders = ordersRepository.findById(ordersResponseDto.getOrderId())
-                .orElseThrow(() -> {
-                    log.warn("[KakaoPay] ready orders not found ordersId = {}",
-                            ordersResponseDto.getOrderId());
-                    return new OrdersNotFoundException("orders not found");
-                });
+        Orders orders = getOrder(orderId);
 
         KakaoReadyRequest kakaoReadyRequest = KakaoReadyRequest.builder()
                 .cid(kakaoPayProperties.getCid())
@@ -91,6 +80,7 @@ public class KakaoPayService {
     }
 
     // ready 이후 받은 토큰으로 approve 요청
+    @Transactional
     public KakaoApproveResponse approve(String pg_token, Long orderId) {
         log.info("[KakaoPay] approve start ordersId = {}",
                 orderId);
@@ -98,12 +88,7 @@ public class KakaoPayService {
         // 상태를 원자적으로 approving 으로 변경
         kakaoPayStateService.markApproving(orderId);
 
-        Orders orders = ordersRepository.findById(orderId)
-                .orElseThrow(() -> {
-                    log.warn("[KakaoPay] approve orders not found ordersId = {}",
-                            orderId);
-                    return new OrdersNotFoundException("orders not found");
-                });
+        Orders orders = getOrder(orderId);
 
         KakaoTid kakaoTid = tidRepository.findByOrders(orders)
                 .orElseThrow(() -> {
@@ -142,19 +127,15 @@ public class KakaoPayService {
     }
 
     // approve 전에 카카오페이 서버에서 cancel url 로 redirect 한 경우
+    @Transactional
     public void cancelBeforePay(Long orderId) {
         log.info("[KakaoPay] cancel before pay start ordersId = {}",
                 orderId);
 
-       ordersRepository.findById(orderId)
-                .orElseThrow(() -> {
-                    log.warn("[KakaoPay] cancel before pay orders not found ordersId = {}",
-                            orderId);
-                    return new OrdersNotFoundException("orders not found");
-                });
+        Orders orders = getOrder(orderId);
 
         // 상태를 원자적으로 cancel 로 변경
-        kakaoPayStateService.markCancel(orderId);
+        kakaoPayStateService.markCancelBeforePay(orderId);
 
         // 아직 결제가 이루어지지 않아서 order 의 상태만 바꿔준다
         log.info("[KakaoPay] cancel before pay success ordersId = {}",
@@ -163,22 +144,14 @@ public class KakaoPayService {
 
     // approve 후에 사용자가 환불을 요구한 경우
     // 전체 취소만 구현
+    @Transactional
     public OrdersCancelResponse cancelAfterPay(Long orderId) {
         log.info("[KakaoPay] cancel after pay start ordersId = {}",
                 orderId);
 
-        Orders orders = ordersRepository.findById(orderId)
-                .orElseThrow(() -> {
-                    log.warn("[KakaoPay] cancel after pay orders not found ordersId = {}",
-                            orderId);
-                    return new OrdersNotFoundException("orders not found");
-                });
+        Orders orders = getOrder(orderId);
 
-        if(orders.getStatus() != OrderStatus.PAID){
-            log.warn("[KakaoPay] cancel after pay orders cant cancel because not paid ordersId = {}",
-                    orderId);
-            throw new KakaoPayCantCancelException("order is not paid");
-        }
+        kakaoPayStateService.markCancelAfterPay(orderId);
 
         // 이미 결제가 이루어진 상황이므로 카카오페이에 cancel api 를 보낸다.
         // 카카오페이에 cancel api 를 보내고 확인 응답을 받고 우리 서비스의 취소내역을 기록한다.
@@ -195,25 +168,10 @@ public class KakaoPayService {
             log.info("[KakaoPay] cancel after pay success orderId={}", orders.getId());
 
             // response 가 제대로 온 경우 -> 카카오페이에서 환불이 이루어진 상황
-            // 이시점에 우리 서비스의 결제 취소를 업데이트 해준다.
-            orders.changeStatus(OrderStatus.CANCELED);
+        } catch (RestClientException e) {
+            log.warn("[KakaoPay] cancel after pay fail orderId={}", orderId, e);
 
-        } catch (HttpStatusCodeException e) {
-
-            String body = e.getResponseBodyAsString();
-
-            try {
-                KakaoErrorResponse error =
-                        objectMapper.readValue(body, KakaoErrorResponse.class);
-
-                log.warn("[KakaoPay] cancel after pay kakao server error ordersId = {}",
-                        orderId);
-                throw new KakaoPayCantCancelException(error.getMessage());
-            } catch (IOException e1) {
-                log.warn("[KakaoPay] cancel after pay response parsing error orderId = {}",
-                        orderId);
-                throw new KakaoPayCantCancelException("kakao response parsing error");
-            }
+            throw new KakaoPayCantCancelException("kakao pay cancel fail");
         }
 
         // 취소한 금액 등 응답을 return 해준다.
@@ -230,26 +188,26 @@ public class KakaoPayService {
 
     // fail redirect 가 오는 경우 아직 결제를 하지 않은 상태
     // 우리 서비스의 주문 상태만 변경해준다.
+    @Transactional
     public void fail(Long orderId){
         log.info("[KakaoPay] fail start ordersId = {}",
                 orderId);
 
-        Orders orders = ordersRepository.findById(orderId)
+        getOrder(orderId);
+
+        kakaoPayStateService.markFail(orderId);
+
+        log.info("[KakaoPay] fail success ordersId = {}",
+                orderId);
+    }
+
+    private Orders getOrder(Long orderId) {
+        return ordersRepository.findById(orderId)
                 .orElseThrow(() -> {
-                    log.warn("[KakaoPay] fail orders not found ordersId = {}",
+                    log.warn("[KakaoPay] cancel after pay orders not found ordersId = {}",
                             orderId);
                     return new OrdersNotFoundException("orders not found");
                 });
-
-        if(orders.getStatus() == OrderStatus.PAID){
-            log.warn("[KakaoPay] fail order cant fail beacause already paid ordersId = {}",
-                    orderId);
-            throw new KakaoPayCantFailException("order is already paid");
-        }
-
-        orders.changeStatus(OrderStatus.FAILED);
-        log.info("[KakaoPay] fail success ordersId = {}",
-                orderId);
     }
 }
 
